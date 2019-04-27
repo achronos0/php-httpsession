be449aa389d07a7590c0883bd70af2f2301108c3
Ky Patterson <ky@morvren-achronos.com>
2019-04-27 14:05:42 -0400

    rename http, add exception class

---
+++ src/Useful/Exception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * \Useful\Exception class
+ *
+ * @link https://github.com/achronos0/useful
+ * @copyright Morvren-Achronos 2019, licensed under Apache 2.0
+ * @package Useful
+ */
+
+/*==NAMESPACE*/
+namespace Useful;
+
+/**
+ * Standard exception thrown by all Useful classes
+ */
+class Exception extends \Exception
+{
+}
+/*NAMESPACE==*/
