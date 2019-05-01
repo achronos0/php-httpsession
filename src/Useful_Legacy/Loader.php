6cc03c2d1247286cdc3554746ac1befb04e46162
Ky Patterson <ky@morvren-achronos.com>
2019-04-30 20:07:10 -0400

    redo legacy loader

--- src/Useful_Legacy/Loader.php
+++ src/Useful_Legacy/Loader.php
@@ -21,6 +21,56 @@ class Useful_Legacy_Loader
 	// Public static
 
 	/**
+	 * Delete previously generated legacy class files
+	 *
+	 * @param string $sNamespace registered namespace prefix, or NULL to clear cache for all registered prefixes
+	 * @return void
+	 */
+	public static function clearCache($sNamespace = null)
+	{
+		if ($sNamespace) {
+			$sNamespace = trim($sNamespace, '\\');
+			$sPrefix = str_replace('\\', '_', $sNamespace) . '_';
+			if (!isset(self::$aRegistry[$sPrefix])) {
+				throw new Exception('Namespace not registered');
+			}
+			self::rmraf(self::$aRegistry[$sPrefix]['cache']);
+			return;
+		}
+		foreach (self::$aRegistry as $aDefine) {
+			self::rmraf($aDefine['cache']);
+		}
+	}
+
+	/**
+	 * Generate a non-namespaced class code file using a namespaced class code file
+	 *
+	 * @param string $sClassFile file containing single class, PSR style
+	 * @param string $sCacheFile file to write modified class code to
+	 * @param bool $bThrow true to throw an Exception if an error occurs during loading
+	 * @return bool true if file written, false on error and if $bThrow is false
+	 * @throws Exception (when requested only)
+	 */
+	protected static function generateClassCacheFile($sClassFile, $sCacheFile, $bThrow = false)
+	{
+		$sClassName = preg_replace('/^(\w+)\..*$/', '$1', $sClassFile);
+		$sCacheDir = dirname($sCacheFile);
+		if (!file_exists($sCacheDir)) {
+			mkdir($sCacheDir, 0777, true);
+		}
+		$sContent = file_get_contents($sClassFile);
+		$sContent = self::transformClassCode($sContent);
+		if (!file_put_contents($sCacheFile, $sContent)) {
+			if ($bThrow) {
+				throw new Exception("file_put_contents($sCacheFile) failed");
+			}
+			return false;
+		}
+		touch($sCacheFile, filemtime($sClassFile));
+		return true;
+	}
+
+	/**
 	 * Load a namespaced class using a non-namespaced classname
 	 *
 	 * This method can be registered directly as an SPL autoloader (and that's exactly what {link @registerSplAutoloader()} does).
@@ -28,7 +78,7 @@ class Useful_Legacy_Loader
 	 * @param string $sClassName legacy classname
 	 * @param bool $bThrow true to throw an Exception if an error occurs during loading
 	 * @return bool true if class loaded, null if class not loaded, false on error and if $bThrow is false
-	 * @throws Exception (on request only)
+	 * @throws Exception (when requested only)
 	 */
 	public static function loadClass($sClassName, $bThrow = false)
 	{
@@ -36,10 +86,10 @@ class Useful_Legacy_Loader
 			$sFind = $aDefine['lc'] ? strtolower($sClassName) : $sClassName;
 			if (strpos($sFind, $sPrefix) === 0) {
 				$sFind = substr($sFind, strlen($sPrefix));
-				$sFilePath = self::findClassFile($sFind, $aDefine['path'], $aDefine['name']);
-				if ($sFilePath) {
-					$sCacheDir = $aDefine['cache'] . dirname(substr($sFilePath, strlen($aDefine['path']))) . DIRECTORY_SEPARATOR;
-					return self::loadClassFromFile($sClassName, $sFilePath, $sCacheDir, $bThrow);
+				$sClassFile = self::findClassFile($sFind, $aDefine['path'], $aDefine['name']);
+				if ($sClassFile) {
+					$sCacheDir = $aDefine['cache'] . dirname(substr($sClassFile, strlen($aDefine['path']))) . DIRECTORY_SEPARATOR;
+					return self::loadClassFromFile($sClassName, $sClassFile, $sCacheDir, $bThrow);
 				}
 			}
 		}
@@ -50,31 +100,29 @@ class Useful_Legacy_Loader
 	 * Load a namespaced class file under a non-namespaced classname
 	 *
 	 * @param string $sLoadAsClassName what to load the class as
-	 * @param string $sFilePath file containing single class, PSR style
+	 * @param string $sClassFile file containing single class, PSR style
 	 * @param string $sCacheDir directory to which modified class code can be written
 	 * @param bool $bThrow true to throw an Exception if an error occurs during loading
 	 * @return bool true if class loaded, false on error and if $bThrow is false
+	 * @throws Exception (when requested only)
 	 */
-	public static function loadClassFromFile($sLoadAsClassName, $sFilePath, $sCacheDir, $bThrow = false)
+	public static function loadClassFromFile($sLoadAsClassName, $sClassFile, $sCacheDir, $bThrow = false)
 	{
-		$sFileName = basename($sFilePath);
-		$sClassName = preg_replace('/^(\w+)\..*$/', '$1', $sFileName);
-		mkdir($sCacheDir, 0777, true);
-		$sCacheFile = $sCacheDir . DIRECTORY_SEPARATOR . $sFileName;
-		if (!file_exists($sCacheFile)) {
-			$sContent = file_get_contents($sFilePath);
-			$sContent = self::transformClassCode($sClassName, $sLoadAsClassName, $sContent);
-			if (!file_put_contents($sCacheFile, $sContent)) {
-				if ($bThrow) {
-					throw new Exception("file_put_contents($sCacheFile) failed");
-				}
+		$sCacheFile = $sCacheDir . DIRECTORY_SEPARATOR . basename($sClassFile);
+		if (!file_exists($sCacheFile) || filemtime($sCacheFile) != filemtime($sClassFile)) {
+			if (!self::generateClassCacheFile($sClassFile, $sCacheFile, $bThrow)) {
 				return false;
 			}
 		}
 		if (!include_once($sCacheFile)) {
-			@unlink($sCacheFile);
 			if ($bThrow) {
 				throw new Exception("include($sCacheFile) failed, check for PHP errors");
+			}
+			return false;
+		}
+		if (!class_exists($sLoadAsClassName, false)) {
+			if ($bThrow) {
+				throw new Exception("class_exists($sLoadAsClassName) failed, class file does not declare correct class name");
 			}
 			return false;
 		}
@@ -137,22 +185,6 @@ class Useful_Legacy_Loader
 		return self::$aRegistry;
 	}
 
-	public static function clearCache($sNamespace = null)
-	{
-		if ($sNamespace) {
-			$sNamespace = trim($sNamespace, '\\');
-			$sPrefix = str_replace('\\', '_', $sNamespace) . '_';
-			if (!isset(self::$aRegistry[$sPrefix])) {
-				throw new Exception('Namespace not registered');
-			}
-			self::rmraf(self::$aRegistry[$sPrefix]['cache']);
-			return;
-		}
-		foreach (self::$aRegistry as $aDefine) {
-			self::rmraf($aDefine['cache']);
-		}
-	}
-
 
 	//////////////////////////////
 	// Internal static
@@ -202,17 +234,111 @@ class Useful_Legacy_Loader
 	 * Transform class file content to load without namespace support
 	 *
 	 * @internal
-	 * @param string $sLoadAsClassName what the class should call itself
 	 * @param string $sContent the original class code
 	 * @return string the transformed class code
 	 */
-	protected static function transformClassCode($sClassName, $sLoadAsClassName, $sContent)
+	protected static function transformClassCode($sContent)
 	{
-		$sContent = str_replace("\n/*==NAMESPACE*/\n", "\n/*==NAMESPACE\n", $sContent);
-		$sContent = str_replace("\n/*NAMESPACE==*/\n", "\n==NAMESPACE*/\n", $sContent);
-		$sContent = preg_replace('/(\n[ \t]*class\s*)[\w\\\\]+/s', '$1' . $sLoadAsClassName, $sContent);
-		$sContent = str_replace("$sClassName::", "$sLoadAsClassName::", $sContent);
+		// Handle namespace statement
+		if (preg_match('/\n[ \t]*namespace\s+([\w\\\\]+)\s*;/s', $sContent, $aMatches)) {
+			$sStatement = substr($aMatches[0], 1);
+			$sNamespace = '\\' . trim($aMatches[1], '\\') . '\\';
+			$sContent = str_replace($sStatement, '/*#nonamespace ' . $sStatement . '*/', $sContent);
+		}
+		else {
+			$sNamespace = '\\';
+		}
+
+		// Handle use statements
+		$aClassConvertMap = array();
+		preg_match_all('/\n[ \t]*use\s+([^;]+);/s', $sContent, $aMatchesList, PREG_SET_ORDER);
+		foreach ($aMatchesList as $aMatches) {
+			$sStatement = substr($aMatches[0], 1);
+			foreach (preg_split('/\s*,\s*/', $aMatches[1]) as $sUse) {
+				if (preg_match('/^([\w\\\\]+)\s+as\s+(\w+)$/', $sUse, $aMatches)) {
+					$sFullClass = $aMatches[1];
+					$sClass = $aMatches[2];
+				}
+				else {
+					$sFullClass = $sUse;
+					$i = strrpos($sFullClass, '\\');
+					if ($i !== false) {
+						$sClass = substr($sFullClass, $i + 1);
+					}
+					else {
+						$sClass = $sFullClass;
+					}
+				}
+				if (substr($sFullClass, 0, 1) != '\\') {
+					$sFullClass = '\\' . $sFullClass;
+				}
+				$aClassConvertMap[$sClass] = $sFullClass;
+			}
+			$sContent = str_replace($sStatement, '/*#nonamespace ' . $sStatement . '*/', $sContent);
+		}
+
+		// Convert class statements
+		preg_match_all('/\n([ \t]*(?:abstract\s+|final\s+)?(?:class\s+|interface\s+))([A-Z]\w*)(?:(\s+extends\s+)([\w\\\\]+))?(?:(\s+implements\s+)([^{]+))?/s', $sContent, $aMatchesList, PREG_SET_ORDER);
+		foreach ($aMatchesList as $aMatches) {
+			$sStatement = substr($aMatches[0], 1);
+			$sPrefix = $aMatches[1];
+			$sClass = $aMatches[2];
+			$sExtendsKeyword = isset($aMatches[3]) ? $aMatches[3] : null;
+			$sExtendsClass = isset($aMatches[4]) ? $aMatches[4] : null;
+			$sImplementsKeyword = isset($aMatches[5]) ? $aMatches[5] : null;
+			$sImplementsClassList = isset($aMatches[6]) ? $aMatches[6] : null;
+			$sClass = self::convertNsClassName($sClass, $sNamespace, $aClassConvertMap);
+			$sNewStatement = $sPrefix . $sClass;
+			if ($sExtendsKeyword) {
+				$sNewStatement .= $sExtendsKeyword . self::convertNsClassName($sExtendsClass, $sNamespace, $aClassConvertMap);
+			}
+			if ($sImplementsKeyword) {
+				foreach (preg_split('/\s*,\s*/', $sImplementsClassList) as $sImplementsClass) {
+					$sImplementsClassList = str_replace($sImplementsClass, self::convertNsClassName($sImplementsClass, $sNamespace, $aClassConvertMap), $sImplementsClassList);
+				}
+				$sNewStatement .= $sImplementsKeyword . $sImplementsClassList;
+			}
+			$sContent = str_replace($sStatement, $sNewStatement, $sContent);
+		}
+
+		// Convert scope resolution operator lvalue
+		preg_match_all('/\b([A-Z][\w\\\\]+)(\s*::)/', $sContent, $aMatchesList, PREG_SET_ORDER);
+		foreach ($aMatchesList as $aMatches) {
+			$sExpression = $aMatches[0];
+			$sNewExpression = self::convertNsClassName($aMatches[1], $sNamespace, $aClassConvertMap) . $aMatches[2];
+			$sContent = str_replace($sExpression, $sNewExpression, $sContent);
+		}
+
+		// Convert new and instanceof operator rvalue
+		preg_match_all('/\b(new\s+|instanceof\s+)([A-Z][\w\\\\]+)/', $sContent, $aMatchesList, PREG_SET_ORDER);
+		foreach ($aMatchesList as $aMatches) {
+			$sExpression = $aMatches[0];
+			$sNewExpression = $aMatches[1] . self::convertNsClassName($aMatches[2], $sNamespace, $aClassConvertMap);
+			$sContent = str_replace($sExpression, $sNewExpression, $sContent);
+		}
+
+		// Convert absolute classnames everywhere
+		while (preg_match('/\\\\[A-Z][\w\\\\]+/', $sContent, $aMatches)) {
+			$sContent = str_replace($aMatches[0], preg_replace('/\\\\+/', '_', substr($aMatches[0], 1)), $sContent);
+		}
+
 		return $sContent;
+	}
+
+	protected static function convertNsClassName($sClassName, $sNamespace, $aClassConvertMap)
+	{
+		if (in_array($sClassName, array('self', 'parent', 'static'))) {
+			return $sClassName;
+		}
+		if (substr($sClassName, 0, 1) != '\\') {
+			if (isset($aClassConvertMap[$sClassName])) {
+				$sClassName = $aClassConvertMap[$sClassName];
+			}
+			else {
+				$sClassName = $sNamespace . $sClassName;
+			}
+		}
+		return str_replace('\\', '_', trim($sClassName, '\\'));
 	}
 
 	protected static function rmraf($sPath)
