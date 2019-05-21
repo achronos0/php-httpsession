ccc6f847833bc611f7722cf4be3ae0260a8b175f
Ky Patterson <ky@morvren-achronos.com>
2019-05-21 17:34:38 -0400

    make legacy autoloader reusable

---
+++ TODO.md
@@ -0,0 +1,11 @@
+
+## 2019-05 old sysframework classes that can be salvaged
+
+* remotefile
+* remoteshell
+* scheduler
+* taskprocessor (async)
+* alert - simple email alert sending
+* ?mail
+* ?file
+* ?mailbox
--- src/legacy_autoloader.php
+++ src/legacy_autoloader.php
@@ -2,31 +2,91 @@
 /**
  * Autoload Useful classes without namespace support, for PHP 5 < 5.3
  *
- * @link https://github.com/morvren-achronos/useful
+ * @link https://github.com/morvren-achronos/php-useful
  * @copyright Morvren-Achronos 2019, licensed under Apache 2.0
  * @package Useful
  */
 
-// Files are relative to here (Useful package root)
-$sRootDir = dirname(dirname(__FILE__));
+/**
+ * Configure autoloader
+ *
+ * Config keys are:
+ *     array ns - Map of namespace prefixes and corresponding classpaths.
+ *     string root_dir - Project root directory. Relative paths are resolved from here. Defaults to the directory that contains composer.json.
+ *     string cache_dir - Root directory to store legacy classfiles. Must be writeable by PHP. Defaults to `root_dir` + "/legacy_src"
+ *
+ * @example
+ *     $LEGACY_AUTOLOADER = array(
+ *         'ns' => array(
+ *             'Acme\\Things' => 'vendor/acme/things/src/Acme/Things',
+ *         ),
+ *     );
+ *
+ * @var array
+ */
+global $LEGACY_AUTOLOADER;
 
-// Where to put cached class files
-$sCacheDir = "$sRootDir/lsrc";
+// Get Useful lib root directory
+$sUsefulDir = dirname(dirname(__FILE__));
 
-// The namespaces to autoload, and where to find the classes
-$aNamespaceList = array(
-	'Useful' => "$sRootDir/src/Useful",
-	'Psr\\Log' => "$sRootDir/vendor/psr/log/Psr/Log",
-);
-
-
-////////////////////
-
-// Include Useful_Legacy_Loader class
-require_once('Useful_Legacy/Loader.php');
-
-// Register namespaces for loading
-foreach ($aNamespaceList as $sNamespace => $sSourceDir) {
-	Useful_Legacy_Loader::registerNamespace($sNamespace, $sSourceDir, "$sCacheDir/$sNamespace");
+// Set defaults
+if (!is_array($LEGACY_AUTOLOADER)) {
+	$LEGACY_AUTOLOADER = array();
 }
-Useful_Legacy_Loader::registerSplAutoloader();
+if (empty($LEGACY_AUTOLOADER['root_dir'])) {
+	$LEGACY_AUTOLOADER['root_dir'] = $sUsefulDir;
+	$sDir = dirname($sUsefulDir);
+	while (strlen($sDir) > 2) {
+		if (file_exists("$sDir/composer.json")) {
+			$LEGACY_AUTOLOADER['root_dir'] = $sDir;
+			break;
+		}
+		$sDir = dirname($sDir);
+	}
+}
+if (empty($LEGACY_AUTOLOADER['cache_dir'])) {
+	$LEGACY_AUTOLOADER['cache_dir'] = "$LEGACY_AUTOLOADER[root_dir]/legacy_src";
+}
+if (empty($LEGACY_AUTOLOADER['ns'])) {
+	$LEGACY_AUTOLOADER['ns'] = array();
+}
+if (empty($LEGACY_AUTOLOADER['ns']['Useful'])) {
+	$LEGACY_AUTOLOADER['ns']['Useful'] = "$sUsefulDir/src/Useful";
+}
+if (empty($LEGACY_AUTOLOADER['ns']['Psr\\Log'])) {
+	$LEGACY_AUTOLOADER['ns']['Psr\\Log'] = "$LEGACY_AUTOLOADER[root_dir]/vendor/psr/log/Psr/Log";
+}
+
+// Include class Useful_Legacy_Loader
+require_once("$sUsefulDir/src/Useful_Legacy/Loader.php");
+
+// Register namespaces
+foreach ($LEGACY_AUTOLOADER['ns'] as $sNamespace => $sSourceDir) {
+	if (substr($sNamespace, 0, 1) == '\\') {
+		$sNamespace = substr($sNamespace, 1);
+	}
+	if (substr($sSourceDir, 0, 1) != '/') {
+		$sSourceDir = "$LEGACY_AUTOLOADER[root_dir]/$sSourceDir";
+	}
+	Useful_Legacy_Loader::registerNamespace(
+		$sNamespace,
+		str_replace('\\', '/', $sSourceDir),
+		str_replace('\\', '/', "$LEGACY_AUTOLOADER[cache_dir]/$sNamespace")
+	);
+}
+
+if (function_exists('spl_autoload_register')) {
+	// Install SPL autoloader (PHP 5.1+)
+	Useful_Legacy_Loader::registerSplAutoloader();
+}
+else {
+	// Install __autoload (PHP 5.0)
+	function __autoload($sClassName)
+	{
+		Useful_Legacy_Loader::loadClass($sClassName);
+	}
+}
+
+// Remove global var
+$LEGACY_AUTOLOADER = null;
+unset($LEGACY_AUTOLOADER);
